5d7cf3c1cadd776edb3b285ca7365151df6bae79
Brandon Marquart <127061285+Bmar04@users.noreply.github.com>
2024-10-06 20:24:26 +1300

    Create TwoSums.java

---
+++ TwoSums.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        boolean itemFound = false;
+        int[] output = new int[2];
+        int i = 0;
+        int l = 1;
+        while (!itemFound){
+            if (nums[i] + nums[l] == target){
+                output[0] = i;
+                output[1] = l;
+                itemFound = true;
+                return output;
+            } 
+            else {
+                if (l == nums.length - 1) {
+                    i++;
+                    l = i + 1;
+                }
+                else {
+                    l++;
+                }
+            }
+        }
+        return null;
+    }
+}
